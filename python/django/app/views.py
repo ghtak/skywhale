ab09eecb57c9382e37e24766afbf5429370de538
ghtak <gwonhyeong.tak@gmail.com>
2023-05-29 23:58:25 +0900

    feat: drf api_view test

--- python/django/app/views.py
+++ python/django/app/views.py
@@ -2,16 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
 from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
-@extend_schema(
-    responses={200: "ok"}
-)
 def index(request):
     return HttpResponse("hello app")
 
+@api_view(['GET'])
+def drf_index(request):
+    return Response()
+
 
 routes = [
-    path('', index)
+    path('index/', index),
+    path('drf/', drf_index)
 ]
