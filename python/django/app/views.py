4a74270140e521b8df6233cc99654d144a4b63b6
ghtak <gwonhyeong.tak@gmail.com>
2023-05-29 23:48:45 +0900

    feat: drf-spectacular

--- python/django/app/views.py
+++ python/django/app/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
+from drf_spectacular.utils import extend_schema
 
 # Create your views here.
 
+@extend_schema(
+    responses={200: "ok"}
+)
 def index(request):
     return HttpResponse("hello app")
 
--- python/django/base/urls.py
+++ python/django/base/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from app import route
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularAPIView
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('app/', include(route.routes))
 ]
