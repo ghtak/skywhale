5114a41959f650649a2c15c2cc48bfc391094b05
ghtak <gwonhyeong.tak@gmail.com>
2023-06-26 23:36:28 +0900

    [feat] add dotenv

--- rust/template_axum/src/main.rs
+++ rust/template_axum/src/main.rs
@@ -2,13 +2,19 @@ use axum::{
     routing::get,
     Router
 };
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let port = std::env::var("PORT").expect("PORT must be set.");
+
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    
+
+    let addr = format!("0.0.0.0:{}", port);
     // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&(addr.parse().unwrap()))
         .serve(app.into_make_service())
         .await
         .unwrap();
