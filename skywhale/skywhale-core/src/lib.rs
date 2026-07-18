588e4d208ec3b6c8fb5f1189eb25b690a3bf5c05
gwonhyeong.tak <gwonhyeong.tak@gmail.com>
2026-07-19 00:09:53 +0900

    feat: add core result alias

--- skywhale/skywhale-core/src/lib.rs
+++ skywhale/skywhale-core/src/lib.rs
@@ -1,3 +1,20 @@
 mod error;
 
 pub use error::Error;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use super::{Error, Result};
+
+    #[test]
+    fn result_alias_uses_core_error() {
+        fn fails() -> Result<()> {
+            Err(anyhow::anyhow!("connection refused"))?;
+            Ok(())
+        }
+
+        assert!(matches!(fails(), Err(Error::Other(_))));
+    }
+}
